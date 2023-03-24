b634e07cc952af2776f190fc823e08e21fa2add8
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-03-24 21:32:44 +0100

    minor changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,42 @@
 <script>
     import { _, locale } from "svelte-i18n";
-    import { mode } from './stores/darkModeStore';
+    import { mode } from "./stores/darkModeStore";
 
-    const myPage = document.getElementsByTagName('html');
+    const myPage = document.getElementsByTagName("html");
     const toggleDarkMode = () => {
-        myPage[0].className = $mode == 'dark' ? '' : 'dark';
+        myPage[0].className = $mode == "dark" ? "" : "dark";
         $mode = myPage[0].className;
     };
 
     const changleLanguage = (lang) => {
-        locale.set(lang)
-    }
+        locale.set(lang);
+    };
 </script>
 
 <main class="bg-gray-300 dark:bg-gray-700 h-screen">
     <div class="dark:text-gray-200 text-gray-800">{$_("content")}</div>
     <div class="flex flex-row gap-2">
-        <div class="btn btn-primary" on:click={() => {changleLanguage("fr")}}>french</div>
-        <div class="btn btn-primary" on:click={() => {changleLanguage("en")}}>english</div>
-        <div class="btn btn-secondary" on:click={toggleDarkMode}>toggle dark mode</div>
+        <div
+            class="btn btn-primary"
+            on:click={() => {
+                changleLanguage("fr");
+            }}
+        >
+            french
+        </div>
+        <div
+            class="btn btn-primary"
+            on:click={() => {
+                changleLanguage("en");
+            }}
+        >
+            english
+        </div>
+        <div class="btn btn-secondary" on:click={toggleDarkMode}>
+            toggle dark mode
+        </div>
     </div>
 </main>
 
 <style>
-
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from "./App.svelte";
 import "./locale/i18n";
 
 const app = new App({
-  target: document.body,
+    target: document.body,
 });
 
 export default app;
