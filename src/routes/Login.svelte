238882dde5509bdd9c7e2c0663f4fcfd07e5e4d2
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-03-29 23:38:58 +0100

    minor changes

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -17,12 +17,12 @@
     <div class="w-full max-w-md space-y-8">
         <div>
             <img
-                class="mx-auto h-12 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Your Company"
+                class="mx-auto mb-10 h-16 md:h-24"
+                src="src/assets/innovium_logos/innovium_light.png"
+                alt="Innovium logo with text"
             />
             <div
-                class="-mx-1 mt-6 text-center text-3xl font-bold tracking-tight text-gray-900"
+                class="-mx-1 mt-6 text-center text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100"
             >
                 {$_("login.Sign in to your account")}
             </div>
@@ -40,7 +40,7 @@
                         type="email"
                         autocomplete="email"
                         required
-                        class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-light sm:text-sm sm:leading-6"
                         placeholder={$_("login.Email address")}
                     />
                 </div>
@@ -54,7 +54,7 @@
                         type="password"
                         autocomplete="current-password"
                         required
-                        class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-light sm:text-sm sm:leading-6"
                         placeholder={$_("login.Password")}
                     />
                 </div>
@@ -66,11 +66,11 @@
                         id="remember-me"
                         name="remember-me"
                         type="checkbox"
-                        class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                        class="h-4 w-4 rounded border-gray-300 text-light focus:ring-light"
                     />
                     <label
                         for="remember-me"
-                        class="ml-2 block text-sm text-gray-900"
+                        class="ml-2 block text-sm text-gray-900 dark:text-gray-100"
                         >{$_("login.Remember me")}</label
                     >
                 </div>
@@ -79,13 +79,13 @@
             <div>
                 <button
                     type="submit"
-                    class="group relative flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    class="group relative flex w-full justify-center rounded-md bg-light px-3 py-2 text-sm font-semibold text-white hover:bg-opacity-75 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-light"
                 >
                     <span
                         class="absolute inset-y-0 left-0 flex items-center pl-3"
                     >
                         <svg
-                            class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                            class="h-5 w-5 text-lightest group-hover:text-white"
                             viewBox="0 0 20 20"
                             fill="currentColor"
                             aria-hidden="true"
