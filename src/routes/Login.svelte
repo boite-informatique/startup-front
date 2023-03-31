b327f5cad9d827b031e9c4c21e9aca638df12d8a
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-03-31 15:38:01 +0100

    minor styling changes

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -72,7 +72,7 @@
                         type="email"
                         autocomplete="email"
                         required
-                        class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-light sm:text-sm sm:leading-6"
+                        class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-light dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-200 sm:text-sm sm:leading-6"
                         placeholder={$_("login.Email address")}
                     />
                 </div>
@@ -87,7 +87,7 @@
                         type="password"
                         autocomplete="current-password"
                         required
-                        class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-light sm:text-sm sm:leading-6"
+                        class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-light dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-200 sm:text-sm sm:leading-6"
                         placeholder={$_("login.Password")}
                     />
                 </div>
