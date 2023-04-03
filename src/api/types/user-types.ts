463656f881380469c3d9d15506d5f8d6d16b09b9
ricin9 <ricin9@outlook.com>
2023-04-03 12:44:05 +0100

    added api calls for user and role management

---
+++ src/api/types/user-types.ts
@@ -0,0 +1,41 @@
+import type { Role } from "./role-types";
+
+export type User = {
+    id: number;
+    email: string;
+    password: string;
+    first_name: string;
+    last_name: string;
+    middle_name: string;
+    date_of_birth: Date;
+    location_of_birth: string;
+    sex: "Male" | "Female";
+    type: "Student" | "Teacher" | "Staff";
+};
+
+export type UserWithRelations = User & {
+    roles: Role[];
+    teacher?: Teacher;
+    student?: Student;
+};
+
+type Teacher = {
+    id: number;
+    user_id: number;
+    domain: string;
+    academic_rank: string;
+    establishement: string[];
+};
+
+type Student = {
+    id: number;
+    user_id: number;
+    domain: string;
+    establishement: string;
+    faculty: string | null;
+    departement: string;
+    filiere: string;
+    level: number;
+    section: string;
+    group: number;
+};
