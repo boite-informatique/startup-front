3856861a7964712735adf043988089740ffbb0ca
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-04-15 19:04:43 +0100

    minor changes

--- src/lib/ModifyUserModal.svelte
+++ src/lib/ModifyUserModal.svelte
@@ -246,11 +246,27 @@
                     >
                 {/each}
                 <button
-                    class="btn-ghost btn-active btn text-white opacity-80 hover:opacity-100"
-                    >add roles <div class="ml-[13px] scale-[1.7] font-bold">
-                        +
-                    </div></button
+                    class="btn-ghost btn-active btn flex flex-row items-center justify-between gap-3 text-white opacity-80 hover:opacity-100"
                 >
+                    <div class="mt-[1px]">
+                        {$_("admin.users.modify roles")}
+                    </div>
+                    <svg
+                        class="h-7 w-7"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="1.5"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        aria-hidden="true"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                        />
+                    </svg>
+                </button>
             </div>
         </div>
 
