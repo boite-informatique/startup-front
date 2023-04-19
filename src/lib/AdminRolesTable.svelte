b3beaf6be6ee1e2c0f9c8f5c777d7c496c376bee
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-04-19 14:08:04 +0100

    working on admin/roles

--- src/lib/AdminRolesTable.svelte
+++ src/lib/AdminRolesTable.svelte
@@ -1,6 +1,7 @@
 <script>
     import { createEventDispatcher } from "svelte";
-    import { fetchUserRoles, fetchUsers } from "../api/admin-user";
+    import { fetchUserRoles } from "../api/admin-user";
+    import { fetchRoles } from "../api/admin-role";
     import { _ } from "svelte-i18n";
     import RolesModal from "./RolesModal.svelte";
     import UserTypeModal from "./UserTypeModal.svelte";
@@ -9,11 +10,8 @@
     const dispatch = createEventDispatcher();
 
     let modalState = false;
-    let UserTypeModalState = false;
     let ModifyUserModalState = false;
     let rolesData;
-    let UserTypeModalData;
-    let UserType;
     let ModifyUserModalData = {
         email: "",
         activated: true,
@@ -21,12 +19,7 @@
         roles: [],
     };
 
-    export let filter = {
-        skip: 0,
-        take: 10,
-    };
-
-    $: users = fetchUsers(filter);
+    let roles = fetchRoles();
 </script>
 
 <div class="max-h-[480px] w-full overflow-x-auto rounded-lg scrollbar-hide">
@@ -36,208 +29,73 @@
             <tr class="sticky top-0 z-10 bg-opacity-100 backdrop-blur-md">
                 <th
                     class="bg-slate-300 text-gray-800 transition-all dark:bg-slate-700 dark:text-gray-200"
-                />
-                <th
-                    class="bg-slate-300 text-gray-800 transition-all dark:bg-slate-700 dark:text-gray-200"
-                    >{$_("admin.users.user")}</th
+                    >{$_("admin.roles.role name")}</th
                 >
                 <th
                     class="bg-slate-300 text-gray-800 transition-all dark:bg-slate-700 dark:text-gray-200"
-                    >{$_("admin.users.type")}</th
+                    >{$_("admin.roles.users")}</th
                 >
                 <th
                     class="bg-slate-300 text-gray-800 transition-all dark:bg-slate-700 dark:text-gray-200"
-                    >{$_("admin.users.roles")}</th
-                >
-                <th
-                    class="bg-slate-300 text-gray-800 transition-all dark:bg-slate-700 dark:text-gray-200"
-                    >{$_("admin.users.sex")}</th
-                >
-                <th
-                    class="bg-slate-300 text-gray-800 transition-all dark:bg-slate-700 dark:text-gray-200"
-                    >{$_("admin.users.birthday infos")}</th
+                    >{$_("admin.roles.permissions")}</th
                 >
             </tr>
         </thead>
         <tbody>
-            {#await users}
+            {#await roles}
                 <button class="btn loading">{$_("admin.users.loading")}</button>
             {:then res}
                 {#if res.status >= 200 && res.status < 300}
-                    <div class="hidden">
-                        {res.data.length < 10
-                            ? dispatch("stopPagination", true)
-                            : dispatch("stopPagination", false)}
-                        <!-- added this to indicate that pagination buttons should not show more indexes because we reached the end -->
-                    </div>
-                    {#each res.data as user}
+                    {#each res.data as role}
                         <tr>
-                            <td
-                                class="bg-gray-200 text-gray-800 transition-all dark:bg-gray-800 dark:text-gray-200"
-                            >
-                                {#if user.activated}
-                                    <label>
-                                        <input
-                                            type="checkbox"
-                                            class="checkbox-primary checkbox cursor-default focus:ring-0"
-                                            checked={true}
-                                            on:click={(e) => {
-                                                e.preventDefault();
-                                            }}
-                                        />
-                                    </label>
-                                {:else}
-                                    <input
-                                        type="checkbox"
-                                        class="checkbox cursor-default ring-1 ring-black"
-                                        style="cursor: default;"
-                                        disabled
-                                        checked
-                                    />
-                                {/if}
-                            </td>
                             <!-- svelte-ignore a11y-click-events-have-key-events -->
                             <td
                                 on:click={() => {
                                     ModifyUserModalState = true;
-                                    ModifyUserModalData = user;
+                                    ModifyUserModalData = role;
                                 }}
                                 title={$_(
-                                    "admin.users.click to modify the user's informations"
+                                    "admin.roles.click to modify the role's informations"
                                 )}
                                 class="cursor-pointer bg-gray-200 text-gray-800 transition-all hover:bg-opacity-10 hover:shadow-inner dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-opacity-40 hover:bg-gray-800"
                             >
-                                <div class="flex items-center space-x-3">
-                                    <div class="avatar">
-                                        <div
-                                            class="mask mask-squircle h-12 w-12"
-                                        >
-                                            <img
-                                                src="https://avatars.githubusercontent.com/u/100171494?v=4"
-                                                alt="Avatar of a user"
-                                            />
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="font-bold">
-                                            {user.first_name}
-                                            {user.middle_name}
-                                            {user.last_name}
-                                        </div>
-                                        <div
-                                            class="text-sm font-medium opacity-90"
-                                        >
-                                            {user.email}
-                                        </div>
-                                    </div>
+                                <div class="flex items-center font-bold">
+                                    {role.name}
+                                </div>
+                                <div class="text-sm font-medium opacity-50">
+                                    {$_(
+                                        "admin.roles.click to modify the role's informations"
+                                    )}
                                 </div>
                             </td>
-                            <!-- svelte-ignore a11y-click-events-have-key-events -->
+                            <td
+                                class="bg-gray-200 font-bold text-gray-800 dark:bg-gray-800 dark:text-gray-200"
+                            >
+                                {role._count.users}
+                                {$_("admin.roles.users")}
+                            </td>
                             <td
                                 class="cursor-pointer bg-gray-200 font-bold text-gray-800 transition-all hover:bg-opacity-10 hover:shadow-inner dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-opacity-40 hover:bg-gray-800"
-                                on:click={() => {
-                                    UserTypeModalState = true;
-                                    UserType = user.type;
-                                    if (user.type === "Student") {
-                                        UserTypeModalData = user.student
-                                            ? user.student
-                                            : {};
-                                    } else if (user.type === "Teacher") {
-                                        UserTypeModalData = user.teacher
-                                            ? user.teacher
-                                            : {};
-                                    }
-                                }}
                             >
-                                {$_(
-                                    `admin.users.${`${user.type}`.toLowerCase()}`
-                                )}
-                                <br />
-                                <span class="text-sm font-medium opacity-50"
-                                    >{$_("admin.users.click to see more")}</span
-                                >
-                            </td>
-                            {#await fetchUserRoles(user.id)}
-                                <td
-                                    class="cursor-pointer bg-gray-200 font-bold text-gray-800 transition-all hover:bg-opacity-10 hover:shadow-inner dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-opacity-40 hover:bg-gray-800"
-                                >
-                                    {$_("admin.users.loading")}
-                                    <br />
-                                    <span class="text-sm font-medium opacity-50"
-                                        >{$_(
-                                            "admin.users.click to see more"
-                                        )}</span
-                                    >
-                                </td>
-                            {:then rolesRes}
-                                <!-- svelte-ignore a11y-click-events-have-key-events -->
-                                <td
-                                    class="cursor-pointer bg-gray-200 font-bold text-gray-800 transition-all hover:bg-opacity-10 hover:shadow-inner dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-opacity-40 hover:bg-gray-800"
-                                    on:click={() => {
-                                        modalState = true;
-                                        rolesRes.status >= 200 &&
-                                        rolesRes.status < 300
-                                            ? (rolesData = rolesRes.data)
-                                            : (rolesData = []);
-                                    }}
-                                >
-                                    {rolesRes.status >= 200 &&
-                                    rolesRes.status < 300
-                                        ? rolesRes.data.length
-                                        : 0}
-                                    {$_("admin.users.roles")}
-                                    <br />
-                                    <span class="text-sm font-medium opacity-50"
-                                        >{$_(
-                                            "admin.users.click to see more"
-                                        )}</span
-                                    >
-                                </td>
-                            {:catch error}
-                                <td
-                                    class="cursor-pointer bg-gray-200 font-bold text-gray-800 transition-all hover:bg-opacity-10 hover:shadow-inner dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-opacity-40 hover:bg-gray-800"
-                                >
-                                    <p
-                                        class="text-red-800 dark:text-red-200 capitalize font-semibold"
-                                    >
-                                        {error.message}
-                                    </p>
-                                    <br />
-                                    <span class="text-sm font-medium opacity-50"
-                                        >{$_(
-                                            "admin.users.click to see more"
-                                        )}</span
-                                    >
-                                </td>
-                            {/await}
-                            <td
-                                class="bg-gray-200 capitalize text-gray-800 transition-all dark:bg-gray-800 dark:text-gray-200"
-                                >{$_(
-                                    `admin.users.${`${user.sex}`.toLowerCase()}`
-                                )}</td
-                            >
-                            <td
-                                class="bg-gray-200 text-gray-800 transition-all dark:bg-gray-800 dark:text-gray-200"
-                            >
-                                {user.date_of_birth.split("T")[0]}
-                                <br />
-                                <span class="text-sm font-medium opacity-90"
-                                    >{user.location_of_birth}</span
-                                >
+                                {role._count.permissions}
+                                {$_("admin.roles.permissions")}
+                                <div class="text-sm font-medium opacity-50">
+                                    {$_("admin.users.click to see more")}
+                                </div>
                             </td>
                         </tr>
                     {:else}
                         <p
                             class="text-gray-800 dark:text-gray-200 text-center text-lg capitalize font-semibold"
                         >
-                            {$_("admin.users.no users available")}
+                            {$_("admin.roles.no roles available")}
                         </p>
                     {/each}
                 {:else}
                     <p
                         class="text-gray-800 dark:text-gray-200 text-center text-lg capitalize font-semibold"
                     >
-                        {$_("admin.users.no users available")}
+                        {$_("admin.roles.no roles available")}
                     </p>
                 {/if}
             {:catch error}
@@ -252,7 +110,6 @@
 </div>
 
 <RolesModal {rolesData} bind:modalState />
-<UserTypeModal {UserTypeModalData} {UserType} bind:UserTypeModalState />
 <ModifyUserModal
     on:showIndicator
     {ModifyUserModalData}
--- src/routes/AdminRoles.svelte
+++ src/routes/AdminRoles.svelte
@@ -1,16 +1,7 @@
 <script>
     import Breadcrumb from "../lib/Breadcrumb.svelte";
-    import RoleFilter from "../lib/RoleFilter.svelte";
     import AddRole from "../lib/AddRole.svelte";
     import AdminRolesTable from "../lib/AdminRolesTable.svelte";
-
-    let filter = {
-        skip: 0,
-        take: 10,
-    };
-    let applyFilters = (e) => {
-        filter = e.detail;
-    };
 </script>
 
 <div class="flex w-full flex-col gap-4 p-4 md:gap-5 md:px-12 md:pt-7">
@@ -19,7 +10,6 @@
         class="-mb-2 flex flex-col items-center justify-between gap-2 md:flex-row"
     >
         <AddRole />
-        <RoleFilter on:applyFilters={applyFilters} />
     </div>
-    <AdminRolesTable on:showIndicator {filter} />
+    <AdminRolesTable on:showIndicator />
 </div>
