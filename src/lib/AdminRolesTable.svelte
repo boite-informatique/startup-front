0ad87eb5a0c2cd9a434800234ae00ae5a80707eb
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-04-19 03:14:40 +0100

    started working on Role section in Admin

---
+++ src/lib/AdminRolesTable.svelte
@@ -0,0 +1,263 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    import { fetchUserRoles, fetchUsers } from "../api/admin-user";
+    import { _ } from "svelte-i18n";
+    import RolesModal from "./RolesModal.svelte";
+    import UserTypeModal from "./UserTypeModal.svelte";
+    import ModifyUserModal from "./ModifyUserModal.svelte";
+
+    const dispatch = createEventDispatcher();
+
+    let modalState = false;
+    let UserTypeModalState = false;
+    let ModifyUserModalState = false;
+    let rolesData;
+    let UserTypeModalData;
+    let UserType;
+    let ModifyUserModalData = {
+        email: "",
+        activated: true,
+        password: "",
+        roles: [],
+    };
+
+    export let filter = {
+        skip: 0,
+        take: 10,
+    };
+
+    $: users = fetchUsers(filter);
+</script>
+
+<div class="max-h-[480px] w-full overflow-x-auto rounded-lg scrollbar-hide">
+    <table class="table w-full">
+        <!-- head -->
+        <thead>
+            <tr class="sticky top-0 z-10 bg-opacity-100 backdrop-blur-md">
+                <th
+                    class="bg-slate-300 text-gray-800 transition-all dark:bg-slate-700 dark:text-gray-200"
+                />
+                <th
+                    class="bg-slate-300 text-gray-800 transition-all dark:bg-slate-700 dark:text-gray-200"
+                    >{$_("admin.users.user")}</th
+                >
+                <th
+                    class="bg-slate-300 text-gray-800 transition-all dark:bg-slate-700 dark:text-gray-200"
+                    >{$_("admin.users.type")}</th
+                >
+                <th
+                    class="bg-slate-300 text-gray-800 transition-all dark:bg-slate-700 dark:text-gray-200"
+                    >{$_("admin.users.roles")}</th
+                >
+                <th
+                    class="bg-slate-300 text-gray-800 transition-all dark:bg-slate-700 dark:text-gray-200"
+                    >{$_("admin.users.sex")}</th
+                >
+                <th
+                    class="bg-slate-300 text-gray-800 transition-all dark:bg-slate-700 dark:text-gray-200"
+                    >{$_("admin.users.birthday infos")}</th
+                >
+            </tr>
+        </thead>
+        <tbody>
+            {#await users}
+                <button class="btn loading">{$_("admin.users.loading")}</button>
+            {:then res}
+                {#if res.status >= 200 && res.status < 300}
+                    <div class="hidden">
+                        {res.data.length < 10
+                            ? dispatch("stopPagination", true)
+                            : dispatch("stopPagination", false)}
+                        <!-- added this to indicate that pagination buttons should not show more indexes because we reached the end -->
+                    </div>
+                    {#each res.data as user}
+                        <tr>
+                            <td
+                                class="bg-gray-200 text-gray-800 transition-all dark:bg-gray-800 dark:text-gray-200"
+                            >
+                                {#if user.activated}
+                                    <label>
+                                        <input
+                                            type="checkbox"
+                                            class="checkbox-primary checkbox cursor-default focus:ring-0"
+                                            checked={true}
+                                            on:click={(e) => {
+                                                e.preventDefault();
+                                            }}
+                                        />
+                                    </label>
+                                {:else}
+                                    <input
+                                        type="checkbox"
+                                        class="checkbox cursor-default ring-1 ring-black"
+                                        style="cursor: default;"
+                                        disabled
+                                        checked
+                                    />
+                                {/if}
+                            </td>
+                            <!-- svelte-ignore a11y-click-events-have-key-events -->
+                            <td
+                                on:click={() => {
+                                    ModifyUserModalState = true;
+                                    ModifyUserModalData = user;
+                                }}
+                                title={$_(
+                                    "admin.users.click to modify the user's informations"
+                                )}
+                                class="cursor-pointer bg-gray-200 text-gray-800 transition-all hover:bg-opacity-10 hover:shadow-inner dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-opacity-40 hover:bg-gray-800"
+                            >
+                                <div class="flex items-center space-x-3">
+                                    <div class="avatar">
+                                        <div
+                                            class="mask mask-squircle h-12 w-12"
+                                        >
+                                            <img
+                                                src="https://avatars.githubusercontent.com/u/100171494?v=4"
+                                                alt="Avatar of a user"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div class="font-bold">
+                                            {user.first_name}
+                                            {user.middle_name}
+                                            {user.last_name}
+                                        </div>
+                                        <div
+                                            class="text-sm font-medium opacity-90"
+                                        >
+                                            {user.email}
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <!-- svelte-ignore a11y-click-events-have-key-events -->
+                            <td
+                                class="cursor-pointer bg-gray-200 font-bold text-gray-800 transition-all hover:bg-opacity-10 hover:shadow-inner dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-opacity-40 hover:bg-gray-800"
+                                on:click={() => {
+                                    UserTypeModalState = true;
+                                    UserType = user.type;
+                                    if (user.type === "Student") {
+                                        UserTypeModalData = user.student
+                                            ? user.student
+                                            : {};
+                                    } else if (user.type === "Teacher") {
+                                        UserTypeModalData = user.teacher
+                                            ? user.teacher
+                                            : {};
+                                    }
+                                }}
+                            >
+                                {$_(
+                                    `admin.users.${`${user.type}`.toLowerCase()}`
+                                )}
+                                <br />
+                                <span class="text-sm font-medium opacity-50"
+                                    >{$_("admin.users.click to see more")}</span
+                                >
+                            </td>
+                            {#await fetchUserRoles(user.id)}
+                                <td
+                                    class="cursor-pointer bg-gray-200 font-bold text-gray-800 transition-all hover:bg-opacity-10 hover:shadow-inner dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-opacity-40 hover:bg-gray-800"
+                                >
+                                    {$_("admin.users.loading")}
+                                    <br />
+                                    <span class="text-sm font-medium opacity-50"
+                                        >{$_(
+                                            "admin.users.click to see more"
+                                        )}</span
+                                    >
+                                </td>
+                            {:then rolesRes}
+                                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                                <td
+                                    class="cursor-pointer bg-gray-200 font-bold text-gray-800 transition-all hover:bg-opacity-10 hover:shadow-inner dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-opacity-40 hover:bg-gray-800"
+                                    on:click={() => {
+                                        modalState = true;
+                                        rolesRes.status >= 200 &&
+                                        rolesRes.status < 300
+                                            ? (rolesData = rolesRes.data)
+                                            : (rolesData = []);
+                                    }}
+                                >
+                                    {rolesRes.status >= 200 &&
+                                    rolesRes.status < 300
+                                        ? rolesRes.data.length
+                                        : 0}
+                                    {$_("admin.users.roles")}
+                                    <br />
+                                    <span class="text-sm font-medium opacity-50"
+                                        >{$_(
+                                            "admin.users.click to see more"
+                                        )}</span
+                                    >
+                                </td>
+                            {:catch error}
+                                <td
+                                    class="cursor-pointer bg-gray-200 font-bold text-gray-800 transition-all hover:bg-opacity-10 hover:shadow-inner dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-opacity-40 hover:bg-gray-800"
+                                >
+                                    <p
+                                        class="text-red-800 dark:text-red-200 capitalize font-semibold"
+                                    >
+                                        {error.message}
+                                    </p>
+                                    <br />
+                                    <span class="text-sm font-medium opacity-50"
+                                        >{$_(
+                                            "admin.users.click to see more"
+                                        )}</span
+                                    >
+                                </td>
+                            {/await}
+                            <td
+                                class="bg-gray-200 capitalize text-gray-800 transition-all dark:bg-gray-800 dark:text-gray-200"
+                                >{$_(
+                                    `admin.users.${`${user.sex}`.toLowerCase()}`
+                                )}</td
+                            >
+                            <td
+                                class="bg-gray-200 text-gray-800 transition-all dark:bg-gray-800 dark:text-gray-200"
+                            >
+                                {user.date_of_birth.split("T")[0]}
+                                <br />
+                                <span class="text-sm font-medium opacity-90"
+                                    >{user.location_of_birth}</span
+                                >
+                            </td>
+                        </tr>
+                    {:else}
+                        <p
+                            class="text-gray-800 dark:text-gray-200 text-center text-lg capitalize font-semibold"
+                        >
+                            {$_("admin.users.no users available")}
+                        </p>
+                    {/each}
+                {:else}
+                    <p
+                        class="text-gray-800 dark:text-gray-200 text-center text-lg capitalize font-semibold"
+                    >
+                        {$_("admin.users.no users available")}
+                    </p>
+                {/if}
+            {:catch error}
+                <p
+                    class="text-red-800 dark:text-red-200 text-center text-lg capitalize font-semibold"
+                >
+                    {error.message}
+                </p>
+            {/await}
+        </tbody>
+    </table>
+</div>
+
+<RolesModal {rolesData} bind:modalState />
+<UserTypeModal {UserTypeModalData} {UserType} bind:UserTypeModalState />
+<ModifyUserModal
+    on:showIndicator
+    {ModifyUserModalData}
+    email={ModifyUserModalData.email}
+    activated={ModifyUserModalData.activated}
+    roles={ModifyUserModalData.roles}
+    bind:ModifyUserModalState
+/>
---
+++ src/lib/RoleFilter.svelte
@@ -0,0 +1,159 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    import { _ } from "svelte-i18n";
+
+    const dispatch = createEventDispatcher();
+
+    type FetchUsersQuery = {
+        skip?: number;
+        take?: number;
+        first_name?: string;
+        sex?: "Male" | "Female";
+        type?: "Teacher" | "Student" | "Staff";
+    };
+
+    let firstName: string = "";
+    let sex: "Male" | "Female";
+    let type: "Teacher" | "Student" | "Staff";
+
+    let applyFilters = () => {
+        filter = {
+            skip: 0,
+            take: 10,
+            first_name: firstName,
+            sex: sex,
+            type: type,
+        };
+
+        dispatch("applyFilters", filter);
+    };
+
+    let filter: FetchUsersQuery = {};
+</script>
+
+<label
+    for="my-modal-5"
+    class="flex w-full max-w-xs cursor-pointer select-none flex-row items-center justify-between rounded-lg bg-gray-200 py-3 pl-4 text-sm font-semibold capitalize text-gray-800 outline outline-1 outline-accent active:outline-4 dark:bg-gray-800 dark:text-gray-200"
+>
+    <div>{$_("admin.roles.filter roles by...")}</div>
+    <svg
+        class="h-7 w-7 scale-150 pr-3"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="1.5"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+    >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+        />
+    </svg>
+</label>
+
+<input type="checkbox" id="my-modal-5" class="modal-toggle" />
+<div class="modal">
+    <div
+        class="modal-box flex w-11/12 max-w-5xl flex-col gap-5 border border-gray-400 text-gray-200"
+    >
+        <h3 class="mb-2 text-xl font-bold capitalize">
+            {$_("admin.users.filter.filter users as you like")}!
+        </h3>
+        <div
+            class="flex flex-col items-start justify-start gap-2 md:flex-row md:items-center"
+        >
+            <div class="text-lg font-semibold capitalize md:w-40">
+                {$_("admin.users.first name")} :
+            </div>
+            <input
+                bind:value={firstName}
+                type="text"
+                placeholder={$_("admin.users.filter.Type here")}
+                class="input-bordered input w-full max-w-xs"
+            />
+        </div>
+        <div
+            class="flex flex-col items-start justify-start gap-2 md:flex-row md:items-center"
+        >
+            <div class="text-lg font-semibold capitalize md:w-40">
+                {$_("admin.users.sex")} :
+            </div>
+            <div class="flex flex-row gap-1 md:gap-2">
+                <button
+                    class="btn {sex == 'Male' ? 'btn-primary' : ''}"
+                    on:click={() => {
+                        sex = "Male";
+                    }}>{$_("admin.users.male")}</button
+                >
+                <button
+                    class="btn {sex == 'Female' ? 'btn-primary' : ''}"
+                    on:click={() => {
+                        sex = "Female";
+                    }}>{$_("admin.users.female")}</button
+                >
+            </div>
+        </div>
+        <div
+            class="flex flex-col items-start justify-start gap-2 md:flex-row md:items-center"
+        >
+            <div class="text-lg font-semibold capitalize md:w-40">
+                {$_("admin.users.type")} :
+            </div>
+            <div class="flex flex-row gap-1 md:gap-2">
+                <button
+                    class="btn {type == 'Student' ? 'btn-primary' : ''}"
+                    on:click={() => {
+                        type = "Student";
+                    }}>{$_("admin.users.student")}</button
+                >
+                <button
+                    class="btn {type == 'Teacher' ? 'btn-primary' : ''}"
+                    on:click={() => {
+                        type = "Teacher";
+                    }}>{$_("admin.users.teacher")}</button
+                >
+                <button
+                    class="btn {type == 'Staff' ? 'btn-primary' : ''}"
+                    on:click={() => {
+                        type = "Staff";
+                    }}>{$_("admin.users.staff")}</button
+                >
+            </div>
+        </div>
+        <div
+            class="mt-3 flex flex-col items-start justify-start gap-2 md:flex-row md:items-center md:justify-between"
+        >
+            <button
+                class="btn -mb-6"
+                on:click={() => {
+                    firstName = "";
+                    sex = undefined;
+                    type = undefined;
+                }}>{$_("admin.users.filter.remove all filters")}</button
+            >
+            <div
+                class="flex flex-row items-center justify-start gap-1 md:gap-2"
+            >
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <label
+                    for="my-modal-5"
+                    class="modal-action btn"
+                    on:click={() => {
+                        firstName = "";
+                        sex = undefined;
+                        type = undefined;
+                    }}>{$_("admin.users.filter.cancel")}</label
+                >
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <label
+                    for="my-modal-5"
+                    class="modal-action btn"
+                    on:click={applyFilters}
+                    >{$_("admin.users.filter.apply filters")}</label
+                >
+            </div>
+        </div>
+    </div>
+</div>
