62d56a64fae7926458c18c68b102cd62232832bf
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-04-26 15:57:52 +0100

    started working on projects for all actors

--- src/routes/MyProject.svelte
+++ src/routes/MyProject.svelte
@@ -1,1 +1,37 @@
+<script>
+    // importing modules
+    import { _ } from "svelte-i18n";
+    import { onMount } from "svelte";
+    import { useNavigate } from "svelte-navigator";
+    import { createEventDispatcher } from "svelte";
+
+    // importing stores
+    import { userPermissions } from "../stores/userPermissions";
+
+    const dispatch = createEventDispatcher();
+
+    const navigate = useNavigate();
+
+    let indicateUnauthorizedToAccessMyProjects = () => {
+        dispatch("showIndicator", {
+            indicatorType: "btn-error",
+            indicatorContent: $_(
+                "projects.indicator.You are not authorized to access '/my projects' route"
+            ),
+            indicatorVisible: true,
+        });
+    };
+
+    onMount(() => {
+        let userIsStudent = $userPermissions.some(
+            (obj) => obj.name === "isStudent"
+        );
+
+        if (!userIsStudent) {
+            indicateUnauthorizedToAccessMyProjects();
+            navigate("/");
+        }
+    });
+</script>
+
 this is my project for project holder (student)
--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -1,1 +1,40 @@
+<script>
+    // importing modules
+    import { _ } from "svelte-i18n";
+    import { onMount } from "svelte";
+    import { useNavigate } from "svelte-navigator";
+    import { createEventDispatcher } from "svelte";
+
+    // importing stores
+    import { userPermissions } from "../stores/userPermissions";
+
+    const dispatch = createEventDispatcher();
+
+    const navigate = useNavigate();
+
+    let indicateUnauthorizedToAccessProjects = () => {
+        dispatch("showIndicator", {
+            indicatorType: "btn-error",
+            indicatorContent: $_(
+                "projects.indicator.You are not authorized to access '/projects' route"
+            ),
+            indicatorVisible: true,
+        });
+    };
+
+    onMount(() => {
+        let userIsAdmin = $userPermissions.some(
+            (obj) => obj.name === "canManageAll"
+        );
+        let userIsTeacher = $userPermissions.some(
+            (obj) => obj.name === "isTeacher"
+        );
+
+        if (!(userIsAdmin || userIsTeacher)) {
+            indicateUnauthorizedToAccessProjects();
+            navigate("/");
+        }
+    });
+</script>
+
 this is projects for teacher or admin
