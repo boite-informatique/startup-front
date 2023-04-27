151a4eacd6054115f63883a6be9f520a5484e6ac
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-04-27 13:20:43 +0100

    finished ProjectValidationModal

--- src/lib/ProjectValidationModal.svelte
+++ src/lib/ProjectValidationModal.svelte
@@ -18,11 +18,26 @@
             class="btn-sm btn-circle btn absolute right-2 top-2 border border-gray-500 text-gray-200"
             >✕</label
         >
-        <h3 class="mb-3 text-lg font-bold capitalize">
-            {$_("projects.the project validation state")}
-        </h3>
-        <div class="">
-            {projectValidationModalData}
-        </div>
+
+        {#if Object.keys(projectValidationModalData).length > 0}
+            <h3 class="text-lg font-bold capitalize">
+                {$_("projects.informations about the project validation state")}
+            </h3>
+            <div class="flex flex-col py-4">
+                {#each Object.entries(projectValidationModalData) as [key, value]}
+                    {#if value}
+                        <div class="text-lg font-medium capitalize">
+                            - {key} : {value}
+                        </div>
+                    {/if}
+                {/each}
+            </div>
+        {:else}
+            <h3 class="text-lg font-bold capitalize">
+                {$_(
+                    "projects.no informations about the project validation state"
+                )}
+            </h3>
+        {/if}
     </div>
 </div>
