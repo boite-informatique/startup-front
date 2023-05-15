f976c3b59ecb81423c0d9c47131b85f0d4da351a
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-05-15 12:54:16 +0100

    added locales for register

--- src/routes/Register.svelte
+++ src/routes/Register.svelte
@@ -146,7 +146,7 @@
 <div
     class="relative flex min-h-screen w-full items-center justify-center justify-self-center overflow-hidden px-4 py-12 sm:px-6 lg:px-8"
 >
-    <!-- <MesrsLogo /> -->
+    <MesrsLogo />
     <div class="absolute top-5 right-5 flex gap-4">
         <DarkModeTogglerLogin />
         <LanguageMenuLogin />
@@ -212,7 +212,7 @@
                     placeholder={$_("register.phone")}
                 />
                 <label for="date_of_birth">
-                    <span class="label-text">{$_("register.date.label")}</span
+                    <span class="label-text">{$_("register.dateLabel")}</span
                     ></label
                 >
                 <input
@@ -252,7 +252,7 @@
                         bind:value={registration_num}
                         type="text"
                         required
-                        class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-light dark:bg-gray-700 dark:text-white dark:ring-gray-400 dark:placeholder:text-gray-200 sm:text-sm sm:leading-6"
+                        class="relative block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-light dark:bg-gray-700 dark:text-white dark:ring-gray-400 dark:placeholder:text-gray-200 sm:text-sm sm:leading-6"
                         placeholder={$_("register.registrationnum")}
                     />
                     <SelectSearch
@@ -277,7 +277,7 @@
                         bind:value={registration_num}
                         type="text"
                         required
-                        class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-light dark:bg-gray-700 dark:text-white dark:ring-gray-400 dark:placeholder:text-gray-200 sm:text-sm sm:leading-6"
+                        class="relative block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-light dark:bg-gray-700 dark:text-white dark:ring-gray-400 dark:placeholder:text-gray-200 sm:text-sm sm:leading-6"
                         placeholder={$_("register.registrationnum")}
                     />
                     <SelectSearch
