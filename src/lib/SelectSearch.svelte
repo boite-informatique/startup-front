582408377d27f60d0233f9a31c839b52118882ed
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-05-15 19:03:46 +0100

    minor changes

--- src/lib/SelectSearch.svelte
+++ src/lib/SelectSearch.svelte
@@ -33,13 +33,13 @@
 
 <div>
     <label for="search-field" class="label-text"
-        >{$_("register.establishment")}</label
+        >{$_("register.establishment name")}</label
     >
     <input
         type="text"
         id="search-field"
         class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-light dark:bg-gray-700 dark:text-white dark:ring-gray-400 dark:placeholder:text-gray-200 sm:text-sm sm:leading-6"
-        placeholder={$_("register.establishment.label")}
+        placeholder={$_("register.establishment")}
         on:input={debounce(handleInput, 200, { maxWait: 1000 })}
     />
 
