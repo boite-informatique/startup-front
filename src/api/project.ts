430cc31283470643d4576f548e8a01699a6abba1
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-05-16 14:14:39 +0100

    added getProjectByID function

--- src/api/project.ts
+++ src/api/project.ts
@@ -18,6 +18,20 @@ export const getProjects = async (): Promise<
     }
 };
 
+export const getProjectByID = async (
+    ID: string
+): Promise<AxiosResponse<GetProjectOutput>> => {
+    try {
+        const response: AxiosResponse<GetProjectOutput> =
+            (await api.getWithQuery("/projects", {
+                ID,
+            })) as unknown as AxiosResponse<GetProjectOutput>;
+        return response;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
 // export const getProjects = async () => {
 //     // this returns an array of the projects that the user has access to, if he is supervisor he will get all projects he supervises, if he is project holder he will get an array of only on project, admin will get all projects
 //     try {
