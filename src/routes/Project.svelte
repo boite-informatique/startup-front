792877914d4122e5b77d83b449ca1b485609cc6c
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-05-17 08:51:20 +0100

    added some styling to Project page

--- src/routes/Project.svelte
+++ src/routes/Project.svelte
@@ -11,10 +11,11 @@
 {:then res}
     {#if res.status >= 200 && res.status < 300}
         <!-- main content goes here -->
-        <div class="flex flex-col">
-            {console.log(res.data)}
+        <div
+            class="flex flex-col gap-5 capitalize text-xl text-gray-800 dark:text-gray-200"
+        >
             <div class="flex flex-row">
-                <div class="w-44">
+                <div class="w-72 font-bold">
                     {$_("projects.product")} :
                 </div>
                 <div>
@@ -22,7 +23,7 @@
                 </div>
             </div>
             <div class="flex flex-row">
-                <div class="w-44">
+                <div class="w-72 font-bold">
                     {$_("projects.brand")} :
                 </div>
                 <div>
@@ -30,7 +31,7 @@
                 </div>
             </div>
             <div class="flex flex-row">
-                <div class="w-44">
+                <div class="w-72 font-bold">
                     {$_("projects.type")} :
                 </div>
                 <div>
@@ -38,7 +39,7 @@
                 </div>
             </div>
             <div class="flex flex-row">
-                <div class="w-44">
+                <div class="w-72 font-bold">
                     {$_("projects.resume")} :
                 </div>
                 <div>
@@ -46,7 +47,7 @@
                 </div>
             </div>
             <div class="flex flex-row">
-                <div class="w-44">
+                <div class="w-72 font-bold">
                     {$_("projects.owner")} :
                 </div>
                 <div class="flex items-center space-x-3">
@@ -70,55 +71,63 @@
                 </div>
             </div>
             <div class="flex flex-row">
-                <div class="w-44">
+                <div class="w-72 font-bold">
                     {$_("projects.members")} :
                 </div>
-                <div class="flex items-center space-x-3">
-                    <div class="avatar">
-                        <div class="mask mask-squircle h-12 w-12">
-                            <img
-                                src="https://avatars.githubusercontent.com/u/100171494?v=4"
-                                alt="Avatar of a user"
-                            />
+                <div class="flex flex-col md:flex-row gap-7">
+                    {#each res.data.members as member}
+                        <div class="flex items-center space-x-3">
+                            <div class="avatar">
+                                <div class="mask mask-squircle h-12 w-12">
+                                    <img
+                                        src="https://avatars.githubusercontent.com/u/100171494?v=4"
+                                        alt="Avatar of a user"
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <div class="font-bold">
+                                    {member.first_name}
+                                    {member.last_name}
+                                </div>
+                                <div class="text-sm font-medium opacity-90">
+                                    {member.email}
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <div class="font-bold">
-                            {res.data.owner.first_name}
-                            {res.data.owner.last_name}
-                        </div>
-                        <div class="text-sm font-medium opacity-90">
-                            {res.data.owner.email}
-                        </div>
-                    </div>
+                    {/each}
                 </div>
             </div>
             <div class="flex flex-row">
-                <div class="w-44">
+                <div class="w-72 font-bold">
                     {$_("projects.supervisors")} :
                 </div>
-                <div class="flex items-center space-x-3">
-                    <div class="avatar">
-                        <div class="mask mask-squircle h-12 w-12">
-                            <img
-                                src="https://avatars.githubusercontent.com/u/100171494?v=4"
-                                alt="Avatar of a user"
-                            />
+                <div class="flex flex-col md:flex-row gap-7">
+                    {#each res.data.supervisors as supervisor}
+                        <div class="flex items-center space-x-3">
+                            <div class="avatar">
+                                <div class="mask mask-squircle h-12 w-12">
+                                    <img
+                                        src="https://avatars.githubusercontent.com/u/100171494?v=4"
+                                        alt="Avatar of a user"
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <div class="font-bold">
+                                    {supervisor.first_name}
+                                    {supervisor.last_name}
+                                </div>
+                                <div class="text-sm font-medium opacity-90">
+                                    {supervisor.email}
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <div class="font-bold">
-                            {res.data.owner.first_name}
-                            {res.data.owner.last_name}
-                        </div>
-                        <div class="text-sm font-medium opacity-90">
-                            {res.data.owner.email}
-                        </div>
-                    </div>
+                    {/each}
                 </div>
             </div>
             <div class="flex flex-row">
-                <div class="w-44">
+                <div class="w-72 font-bold">
                     {$_("projects.created at")} :
                 </div>
                 <div>
@@ -133,22 +142,30 @@
                 </div>
             </div>
             <div class="flex flex-row">
-                <div class="w-44">
+                <div class="w-72 font-bold">
                     {$_("projects.validation")} :
                 </div>
-                <div>haja</div>
+                <div>
+                    {$_(
+                        "projects.click here to see the project validation state"
+                    )}
+                </div>
             </div>
             <div class="flex flex-row">
-                <div class="w-44">
+                <div class="w-72 font-bold">
                     {$_("projects.project progess")} :
                 </div>
                 <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
                 <div tabindex="0" class="collapse">
-                    <div class="collapse-title text-xl font-medium">
+                    <div
+                        class="collapse-title text-xl font-medium bg-black dark:bg-white bg-opacity-5 dark:bg-opacity-5 rounded-t-xl"
+                    >
                         Focus me to see content
                     </div>
-                    <div class="collapse-content">
-                        <p>
+                    <div
+                        class="collapse-content bg-black dark:bg-white bg-opacity-10 dark:bg-opacity-10 rounded-b-xl pt-2"
+                    >
+                        <p class="-mb-1 mt-1">
                             tabindex="0" attribute is necessary to make the div
                             focusable
                         </p>
@@ -156,16 +173,20 @@
                 </div>
             </div>
             <div class="flex flex-row">
-                <div class="w-44">
+                <div class="w-72 font-bold">
                     {$_("projects.invited users")} :
                 </div>
                 <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
                 <div tabindex="0" class="collapse">
-                    <div class="collapse-title text-xl font-medium">
+                    <div
+                        class="collapse-title text-xl font-medium bg-black dark:bg-white bg-opacity-5 dark:bg-opacity-5 rounded-t-xl"
+                    >
                         Focus me to see content
                     </div>
-                    <div class="collapse-content">
-                        <p>
+                    <div
+                        class="collapse-content bg-black dark:bg-white bg-opacity-10 dark:bg-opacity-10 rounded-b-xl pt-2"
+                    >
+                        <p class="-mb-1 mt-1">
                             tabindex="0" attribute is necessary to make the div
                             focusable
                         </p>
@@ -173,33 +194,20 @@
                 </div>
             </div>
             <div class="flex flex-row">
-                <div class="w-44">
-                    {$_("projects.invited users")} :
-                </div>
-                <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-                <div tabindex="0" class="collapse">
-                    <div class="collapse-title text-xl font-medium">
-                        Focus me to see content
-                    </div>
-                    <div class="collapse-content">
-                        <p>
-                            tabindex="0" attribute is necessary to make the div
-                            focusable
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="flex flex-row">
-                <div class="w-44">
+                <div class="w-72 font-bold">
                     {$_("projects.project history")} :
                 </div>
                 <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
                 <div tabindex="0" class="collapse">
-                    <div class="collapse-title text-xl font-medium">
+                    <div
+                        class="collapse-title text-xl font-medium bg-black dark:bg-white bg-opacity-5 dark:bg-opacity-5 rounded-t-xl"
+                    >
                         Focus me to see content
                     </div>
-                    <div class="collapse-content">
-                        <p>
+                    <div
+                        class="collapse-content bg-black dark:bg-white bg-opacity-10 dark:bg-opacity-10 rounded-b-xl pt-2"
+                    >
+                        <p class="-mb-1 mt-1">
                             tabindex="0" attribute is necessary to make the div
                             focusable
                         </p>
@@ -207,33 +215,20 @@
                 </div>
             </div>
             <div class="flex flex-row">
-                <div class="w-44">
-                    {$_("projects.project history")} :
-                </div>
-                <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-                <div tabindex="0" class="collapse">
-                    <div class="collapse-title text-xl font-medium">
-                        Focus me to see content
-                    </div>
-                    <div class="collapse-content">
-                        <p>
-                            tabindex="0" attribute is necessary to make the div
-                            focusable
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="flex flex-row">
-                <div class="w-44">
+                <div class="w-72 font-bold">
                     {$_("projects.defense authorization")} :
                 </div>
                 <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
                 <div tabindex="0" class="collapse">
-                    <div class="collapse-title text-xl font-medium">
+                    <div
+                        class="collapse-title text-xl font-medium bg-black dark:bg-white bg-opacity-5 dark:bg-opacity-5 rounded-t-xl"
+                    >
                         Focus me to see content
                     </div>
-                    <div class="collapse-content">
-                        <p>
+                    <div
+                        class="collapse-content bg-black dark:bg-white bg-opacity-10 dark:bg-opacity-10 rounded-b-xl pt-2"
+                    >
+                        <p class="-mb-1 mt-1">
                             tabindex="0" attribute is necessary to make the div
                             focusable
                         </p>
@@ -241,16 +236,20 @@
                 </div>
             </div>
             <div class="flex flex-row">
-                <div class="w-44">
+                <div class="w-72 font-bold">
                     {$_("projects.defense documents")} :
                 </div>
                 <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
                 <div tabindex="0" class="collapse">
-                    <div class="collapse-title text-xl font-medium">
+                    <div
+                        class="collapse-title text-xl font-medium bg-black dark:bg-white bg-opacity-5 dark:bg-opacity-5 rounded-t-xl"
+                    >
                         Focus me to see content
                     </div>
-                    <div class="collapse-content">
-                        <p>
+                    <div
+                        class="collapse-content bg-black dark:bg-white bg-opacity-10 dark:bg-opacity-10 rounded-b-xl pt-2"
+                    >
+                        <p class="-mb-1 mt-1">
                             tabindex="0" attribute is necessary to make the div
                             focusable
                         </p>
@@ -258,16 +257,20 @@
                 </div>
             </div>
             <div class="flex flex-row">
-                <div class="w-44">
+                <div class="w-72 font-bold">
                     {$_("projects.defense planification")} :
                 </div>
                 <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
                 <div tabindex="0" class="collapse">
-                    <div class="collapse-title text-xl font-medium">
+                    <div
+                        class="collapse-title text-xl font-medium bg-black dark:bg-white bg-opacity-5 dark:bg-opacity-5 rounded-t-xl"
+                    >
                         Focus me to see content
                     </div>
-                    <div class="collapse-content">
-                        <p>
+                    <div
+                        class="collapse-content bg-black dark:bg-white bg-opacity-10 dark:bg-opacity-10 rounded-b-xl pt-2"
+                    >
+                        <p class="-mb-1 mt-1">
                             tabindex="0" attribute is necessary to make the div
                             focusable
                         </p>
