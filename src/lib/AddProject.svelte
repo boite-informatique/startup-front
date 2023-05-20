d04545d16e5090917097ad39ba026cd70589f2eb
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-05-20 13:53:36 +0100

    added button to delete members

--- src/lib/AddProject.svelte
+++ src/lib/AddProject.svelte
@@ -15,6 +15,14 @@
     let supervisorEmail: string = "";
     let supervisorsEmails: string[] = [];
 
+    let indicateUserAlreadyAdded = () => {
+        dispatch("showIndicator", {
+            indicatorType: "btn-error",
+            indicatorContent: $_("projects.indicator.user already added"),
+            indicatorVisible: true,
+        });
+    };
+
     let handleAddingProject = async (e) => {
         let indicateDataIsMissing = () => {
             dispatch("showIndicator", {
@@ -167,15 +175,44 @@
             </div>
             <div class="flex flex-col gap-[6px]">
                 {#each membersEmails as member}
-                    <input
-                        bind:value={member}
-                        id="email-address"
-                        name="email"
-                        type="email"
-                        autocomplete="email"
-                        placeholder={$_("login.Email address")}
-                        class="input-bordered input w-full max-w-xs md:w-[320px]"
-                    />
+                    <div class="gap2 flex flex-row gap-2">
+                        <input
+                            bind:value={member}
+                            id="email-address"
+                            name="email"
+                            type="email"
+                            autocomplete="email"
+                            placeholder={$_("login.Email address")}
+                            class="input-bordered input w-full max-w-xs md:w-[320px]"
+                        />
+                        <button
+                            class="btn-outline btn-error btn-square btn {membersEmails.length >=
+                            membersLimit
+                                ? 'btn-disabled'
+                                : ' cursor-pointer'}"
+                            on:click={() => {
+                                membersEmails = membersEmails.filter(
+                                    (item) => item !== member
+                                );
+                            }}
+                        >
+                            <svg
+                                fill="none"
+                                class="h-8 w-8"
+                                stroke="currentColor"
+                                stroke-width="1.5"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+                                />
+                            </svg>
+                        </button>
+                    </div>
                 {/each}
                 <div class="flex flex-row gap-2">
                     <input
@@ -193,11 +230,15 @@
                             : ""}
                     >
                         <button
-                            class="btn-square btn {membersEmails.length >=
+                            class="btn-outline btn-success btn-square btn {membersEmails.length >=
                             membersLimit
                                 ? 'btn-disabled'
                                 : ' cursor-pointer'}"
                             on:click={() => {
+                                if (membersEmails.includes(memberEmail)) {
+                                    indicateUserAlreadyAdded();
+                                    return;
+                                }
                                 if (memberEmail.length != 0) {
                                     membersEmails = [
                                         ...membersEmails,
@@ -208,18 +249,20 @@
                             }}
                         >
                             <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                class="h-6 w-6 rotate-45 font-extrabold"
                                 fill="none"
-                                viewBox="0 0 24 24"
+                                class="h-8 w-8"
                                 stroke="currentColor"
-                                ><path
+                                stroke-width="1.5"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                                aria-hidden="true"
+                            >
+                                <path
                                     stroke-linecap="round"
                                     stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M6 18L18 6M6 6l12 12"
-                                /></svg
-                            >
+                                    d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+                                />
+                            </svg>
                         </button>
                     </div>
                     {#if membersEmails.length != 0}
