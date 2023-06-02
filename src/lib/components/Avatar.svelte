40ff90a69b1dd3f1df18fb993c34d26b26e5a2c0
Mohamed Miloudi <ricin9@outlook.com>
2023-06-02 23:20:51 +0100

    forgor to git add . avatar.svelte

---
+++ src/lib/components/Avatar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    const baseURL = import.meta.env.VITE_baseURL;
+
+    export let src = "";
+    export let altText = "";
+</script>
+
+{#if src}
+    <img src={baseURL + "upload/" + src} alt="Avatar of a user" />
+{:else}
+    <div class="placeholder avatar">
+        <div class="h-12 w-12 bg-neutral-focus text-neutral-content">
+            <span class="text-3xl">{altText[0]}</span>
+        </div>
+    </div>
+{/if}
