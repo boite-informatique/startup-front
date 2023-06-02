4d771e6fadef00e94fbd52c2539575bd5c5ae897
Mohamed Miloudi <ricin9@outlook.com>
2023-06-03 00:30:02 +0100

    added image modal component to project history page

--- src/lib/ProjectPage/ProjectHistoryPage.svelte
+++ src/lib/ProjectPage/ProjectHistoryPage.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import type { PopulatedProject } from "src/api/project/types/project-types";
     import { _ } from "svelte-i18n";
+    import ImageModal from "../components/ImageModal.svelte";
 
     export let data: PopulatedProject;
 </script>
@@ -14,78 +15,67 @@
         {$_("projects.project history")} :
     </div>
     <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-    <div tabindex="0" class="collapse w-7/12">
-        <div
-            class="collapse-title rounded-t-xl bg-black bg-opacity-5 text-xl font-medium dark:bg-white dark:bg-opacity-5"
-        >
-            {$_("projects.click here to see the project history")}
-        </div>
-        <div
-            class="collapse-content rounded-b-xl bg-black bg-opacity-10 pt-2 dark:bg-white dark:bg-opacity-10"
-        >
-            {#if data.history}
-                <div class="flex flex-col gap-5">
-                    {#each data.history as ProjectState}
-                        <div class="flex flex-col gap-2">
-                            <div>
-                                - {$_("projects.modified field")} : {ProjectState.field}
-                            </div>
-                            <div>
-                                - {$_("projects.changed at")} : {new Date(
-                                    ProjectState.changed_at
-                                ).toLocaleString("en-UK", {
-                                    year: "numeric",
-                                    month: "long",
-                                    day: "numeric",
-                                    hour: "numeric",
-                                    minute: "numeric",
-                                    timeZone: "UTC",
-                                })}
-                            </div>
-                            <div>
-                                - {$_("projects.old value")} : {ProjectState.old_value}
-                            </div>
-                            <div>
-                                - {$_("projects.new value")} : {ProjectState.new_value}
-                            </div>
-                            <div
-                                class="flex flex-col items-center gap-5 md:flex-row"
+    {#if data.history}
+        <div class="flex flex-col gap-5">
+            {#each data.history as ProjectState}
+                <div class="flex flex-col gap-2">
+                    <div>
+                        <span class="font-bold"
+                            >{$_("projects.modified field")} :
+                        </span>
+                        {ProjectState.field}
+                    </div>
+                    <div>
+                        <span class="font-bold"
+                            >{$_("projects.changed at")} :
+                        </span>
+                        {new Date(ProjectState.changed_at).toLocaleString(
+                            "en-UK",
+                            {
+                                year: "numeric",
+                                month: "long",
+                                day: "numeric",
+                                hour: "numeric",
+                                minute: "numeric",
+                                timeZone: "UTC",
+                            }
+                        )}
+                    </div>
+                    <div class="flex items-center">
+                        <span class="font-bold"
+                            >{$_("projects.old value")} :
+                        </span>
+                        {#if ProjectState.field === "logo"}
+                            <span class="ml-4"
+                                ><ImageModal
+                                    src={ProjectState.old_value}
+                                /></span
                             >
-                                <div>
-                                    - {$_("projects.modified by")} :
-                                </div>
-                                <div class="flex items-center space-x-3">
-                                    <div class="avatar">
-                                        <div
-                                            class="mask mask-squircle h-12 w-12"
-                                        >
-                                            <img
-                                                src="https://avatars.githubusercontent.com/u/100171494?v=4"
-                                                alt="Avatar of a user"
-                                            />
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="font-bold">
-                                            {ProjectState.user?.first_name}
-                                            {ProjectState.user?.last_name}
-                                        </div>
-                                        <div
-                                            class="text-sm font-medium opacity-90"
-                                        >
-                                            {ProjectState.user?.email}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    {:else}
-                        {$_("projects.no history for this project")}
-                    {/each}
+                        {:else}
+                            {ProjectState.old_value}
+                        {/if}
+                    </div>
+                    <div class="flex items-center">
+                        <span class="font-bold"
+                            >{$_("projects.new value")} :
+                        </span>
+                        {#if ProjectState.field === "logo"}
+                            <span class="ml-4"
+                                ><ImageModal
+                                    src={ProjectState.new_value}
+                                /></span
+                            >
+                        {:else}
+                            {ProjectState.new_value}
+                        {/if}
+                    </div>
+                    <div class="divider" />
                 </div>
             {:else}
                 {$_("projects.no history for this project")}
-            {/if}
+            {/each}
         </div>
-    </div>
+    {:else}
+        {$_("projects.no history for this project")}
+    {/if}
 </div>
---
+++ src/lib/components/ImageModal.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    const baseURL = import.meta.env.VITE_baseURL;
+
+    export let src = "";
+    const randomInt = Math.floor(Math.random() * 100000); // hack so we can use multiple modals for same component
+</script>
+
+<!-- The button to open modal -->
+<label for={"my_modal_" + randomInt} class="h-8 w-8 cursor-pointer fill-current"
+    ><svg xmlns="http://www.w3.org/2000/svg" height="2rem" viewBox="0 0 512 512"
+        ><path
+            d="M0 96C0 60.7 28.7 32 64 32H448c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM323.8 202.5c-4.5-6.6-11.9-10.5-19.8-10.5s-15.4 3.9-19.8 10.5l-87 127.6L170.7 297c-4.6-5.7-11.5-9-18.7-9s-14.2 3.3-18.7 9l-64 80c-5.8 7.2-6.9 17.1-2.9 25.4s12.4 13.6 21.6 13.6h96 32H424c8.9 0 17.1-4.9 21.2-12.8s3.6-17.4-1.4-24.7l-120-176zM112 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z"
+        /></svg
+    ></label
+>
+
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id={"my_modal_" + randomInt} class="modal-toggle" />
+<label for={"my_modal_" + randomInt} class="modal z-[9999] cursor-pointer">
+    <label
+        class="modal-box relative overflow-hidden bg-transparent bg-opacity-0 shadow-none blur-none"
+        for=""
+    >
+        <img src={baseURL + "upload/" + src} alt="modal" />
+    </label>
+</label>
