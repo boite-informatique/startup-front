a6b9770a061313a4762b22c140c93d9f97a3abe8
Mohamed Miloudi <ricin9@outlook.com>
2023-06-08 00:49:29 +0100

    added dates to banners, only shows when in a significant period

--- src/lib/PeriodBanner.svelte
+++ src/lib/PeriodBanner.svelte
@@ -1,13 +1,32 @@
 <script>
     import { currentPeriod } from "src/stores/currentPeriod";
+    import { periods } from "src/stores/periodsStore";
     import { _ } from "svelte-i18n";
 
     let message = $currentPeriod;
+    let period;
+    if (message == "Within submission phase") period = $periods.submission;
+    else if (message == "Within validation phase") period = $periods.validation;
+    else if (message == "Within appeal phase") period = $periods.appeal;
+    else if (message == "Within appeal validation phase")
+        period = $periods.appealValidation;
 </script>
 
 <div
-    class="flex h-9 w-full items-center justify-center text-center font-semibold capitalize"
+    class="flex h-9 w-full flex-row items-center justify-between px-2 text-center font-semibold capitalize"
     title={$_(`banner.current period`)}
 >
-    {$_(`banner.${message}`)}
+    {#if period}
+        <div>
+            {period.start}
+        </div>
+    {/if}
+    <div class="mx-auto">
+        {$_(`banner.${message}`)}
+    </div>
+    {#if period}
+        <div>
+            {period.end}
+        </div>
+    {/if}
 </div>
--- src/stores/currentPeriod.ts
+++ src/stores/currentPeriod.ts
@@ -4,8 +4,8 @@ import { periods, type PeriodsType } from "./periodsStore";
 export const currentPeriod = derived<typeof periods, CurrentPeriodType>(
     periods,
     ($periods, set) => {
-        console.log("[PERIODS]", JSON.stringify($periods));
         const currentDate = new Date();
+
         if (currentDate < new Date($periods.submission.start)) {
             set("Platform is not open yet");
         } else if (currentDate <= new Date($periods.submission.end)) {
