9bbedcff86778efa5deb3945d96016f7f56df450
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-06-08 01:28:41 +0100

    working on referential

--- src/routes/Referential.svelte
+++ src/routes/Referential.svelte
@@ -1,159 +1,24 @@
 <script>
     import { _ } from "svelte-i18n";
+    import { Route, navigate, useLocation } from "svelte-navigator";
+
     import Sidebar from "src/lib/components/layout/Sidebar.svelte";
     import { sidebarLists } from "src/config/sidebarList";
+    import CreateReferential from "src/lib/components/CreateReferential.svelte";
+    import DisplayReferential from "src/lib/components/DisplayReferential.svelte";
+
+    const location = useLocation();
+    $: if ($location.pathname == "/referential") {
+        navigate("/referential/create");
+    }
 </script>
 
 <div class="flex h-full w-full flex-1 flex-col lg:flex-row">
     <Sidebar sidebarList={sidebarLists.referential} />
-    <div class="flex w-full flex-col items-start gap-3 p-7 pt-10">
-        <div class="mb-3 text-3xl font-bold">
-            {$_("referential.referential")}
-        </div>
-        <div class="flex flex-row gap-2">
-            <div class="translate-y-2 text-xl font-bold sm:w-[350px]">
-                {$_("referential.add an establishment")} :
-            </div>
-            <div class="flex flex-row gap-2">
-                <input
-                    type="text"
-                    placeholder={$_("admin.users.filter.Type here")}
-                    class="input-bordered input w-full max-w-xs"
-                />
-                <button class="btn-outline btn-square btn" on:click={() => {}}>
-                    <svg
-                        class="h-8 w-8"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                        aria-hidden="true"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M12 4.5v15m7.5-7.5h-15"
-                        />
-                    </svg>
-                </button>
-            </div>
-        </div>
-        <div class="flex flex-row gap-2">
-            <div class="translate-y-2 text-xl font-bold sm:w-[350px]">
-                {$_("referential.add a speciality")} :
-            </div>
-            <div class="flex flex-row gap-2">
-                <input
-                    type="text"
-                    placeholder={$_("admin.users.filter.Type here")}
-                    class="input-bordered input w-full max-w-xs"
-                />
-                <button class="btn-outline btn-square btn" on:click={() => {}}>
-                    <svg
-                        class="h-8 w-8"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                        aria-hidden="true"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M12 4.5v15m7.5-7.5h-15"
-                        />
-                    </svg>
-                </button>
-            </div>
-        </div>
-        <div class="flex flex-row gap-2">
-            <div class="translate-y-2 text-xl font-bold sm:w-[350px]">
-                {$_("referential.add a filiere")} :
-            </div>
-            <div class="flex flex-row gap-2">
-                <input
-                    type="text"
-                    placeholder={$_("admin.users.filter.Type here")}
-                    class="input-bordered input w-full max-w-xs"
-                />
-                <button class="btn-outline btn-square btn" on:click={() => {}}>
-                    <svg
-                        class="h-8 w-8"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                        aria-hidden="true"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M12 4.5v15m7.5-7.5h-15"
-                        />
-                    </svg>
-                </button>
-            </div>
-        </div>
-        <div class="flex flex-row gap-2">
-            <div class="translate-y-2 text-xl font-bold sm:w-[350px]">
-                {$_("referential.add a grade-teacher")} :
-            </div>
-            <div class="flex flex-row gap-2">
-                <input
-                    type="text"
-                    placeholder={$_("admin.users.filter.Type here")}
-                    class="input-bordered input w-full max-w-xs"
-                />
-                <button class="btn-outline btn-square btn" on:click={() => {}}>
-                    <svg
-                        class="h-8 w-8"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                        aria-hidden="true"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M12 4.5v15m7.5-7.5h-15"
-                        />
-                    </svg>
-                </button>
-            </div>
-        </div>
-        <div class="flex flex-row gap-2">
-            <div class="translate-y-2 text-xl font-bold sm:w-[350px]">
-                {$_("referential.add a grade-staff")} :
-            </div>
-            <div class="flex flex-row gap-2">
-                <input
-                    type="text"
-                    placeholder={$_("admin.users.filter.Type here")}
-                    class="input-bordered input w-full max-w-xs"
-                />
-                <button class="btn-outline btn-square btn" on:click={() => {}}>
-                    <svg
-                        class="h-8 w-8"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                        aria-hidden="true"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M12 4.5v15m7.5-7.5h-15"
-                        />
-                    </svg>
-                </button>
-            </div>
-        </div>
-    </div>
+    <Route path="create">
+        <CreateReferential on:showIndicator />
+    </Route>
+    <Route path="display">
+        <DisplayReferential on:showIndicator />
+    </Route>
 </div>
